f4f492f7da11dc39fe7ee06cfb9fb30990e8dcf4
Kevin Cloud <kmc247@pitt.edu>
2021-08-18 17:45:40 -0700

    Update for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pitt_schemas',
-      version='0.1',
+      version='0.11',
       description='Pitt JSON Schemas and validators',
       url='https://github.com/ulsdevteam/pitt_schemas',
       author='University of Pittsburgh Library System, Systems Development',
